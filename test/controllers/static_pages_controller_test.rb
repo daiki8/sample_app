07c65df26fce1f72b0a29072a347f47486bd00ae
daiki8 <daiki1945815@gmail.com>
2018-08-11 05:51:17 +0000

    Update static_pages_controller_test.rb

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -10,13 +10,13 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get help" do
     get static_pages_help_url
     assert_response :success
-    assert_select "title", "help | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
   
   test "should get about" do
     get static_pages_about_url
     assert_response :success
-    assert_select "title", "about | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
 
 end
