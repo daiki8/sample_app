53f0d28b23a34f6858f37c30c6b3a3e739c37889
daiki8 <daiki1945815@gmail.com>
2018-08-13 12:36:26 +0000

    Add password_digest

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User", email: "user@example.com",
+                    password: "foobar", password_confirmation: "foobar")
   end
   
   test "Should be valid" do
