58d6710f8ec249388b8f817ef6b7bf18e68dec77
daiki8 <daiki1945815@gmail.com>
2018-08-13 11:53:26 +0000

    Add validates email uniqueness in models/user.rb

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -46,4 +46,12 @@ class UserTest < ActiveSupport::TestCase
       assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
     end
   end
+  
+  test "email address should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+  
 end
