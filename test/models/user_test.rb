585435e843c35a1747493ce5efc078f2b5baf0ba
daiki8 <daiki1945815@gmail.com>
2018-08-13 09:33:13 +0000

    Add validates presence in user_test.rb

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -8,4 +8,14 @@ class UserTest < ActiveSupport::TestCase
   test "Should be valid" do
     assert @user.valid?
   end
+  
+  test "name should be presence" do
+    @user.name = ""
+    assert_not @user.valid?
+  end
+  
+  test "email should be presence" do
+    @user.email = "    "
+    assert_not @user.valid?
+  end
 end
