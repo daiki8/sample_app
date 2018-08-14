1ffe35c7beb7f560f5397cc5d4a211c99c2bfcaa
daiki8 <daiki1945815@gmail.com>
2018-08-14 07:10:22 +0000

    Updte func new in sessions_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,14 @@
 class SessionsController < ApplicationController
   def new
+    user = User.find_by(email: params[:session][:email],
+                      password: params[:session][:password])
+    if user && user.authenticate(params[:session][:password])
+      #
+    else
+      render 'new'
+    end
+  end
+  
+  def destroy
   end
 end
