da00ceb74899768a78872656d47f356801fc441a
daiki8 <daiki1945815@gmail.com>
2018-08-14 15:33:49 +0000

    Update session

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get signup
+    get signup_path
     assert_response :success
   end
 
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -24,6 +24,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     follow_redirect!
     assert_template "users/show"
-    assert_not flash[:success].empty?
+    assert_not flash.empty?
+    assert is_logged_in?
   end
 end
