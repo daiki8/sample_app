67e5adad2993f5022f6d9d3fbf2d42b0615a5420
daiki8 <daiki1945815@gmail.com>
2018-08-14 15:07:20 +0000

    Update session

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get signup_path
+    get signup
     assert_response :success
   end
 
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @user = users(:michael)
+  end
+  
   test "login with invalid infomation" do
     get login_path
     assert_template "sessions/new"
@@ -9,6 +14,19 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
     get root_path
     assert flash.empty?
+  end
+  
+  test "login with valid infomation" do
+    get login_path
+    assert_template "sessions/new"
+    post login_path, params: { session: { email: @user.email,
+                                          password: "password"}}
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template "users/show"
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
   end
   
 end
