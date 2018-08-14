d80982672216d8e93789afc707df6d3eba67b623
daiki8 <daiki1945815@gmail.com>
2018-08-14 07:28:37 +0000

    Add Test of 'login with invalid infomation'

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,13 @@
 class SessionsController < ApplicationController
   def new
-    user = User.find_by(email: params[:session][:email],
-                      password: params[:session][:password])
+  end
+  
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       #
     else
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
