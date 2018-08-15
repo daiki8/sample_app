a175c23744205d24f37781a949ed12d5417f1600
daiki8 <daiki1945815@gmail.com>
2018-08-15 03:04:52 +0000

    Add func remember in sessions_helper.rb

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,6 +3,12 @@ module SessionsHelper
     session[:user_id] = user.id
   end
   
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user_id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+  
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
