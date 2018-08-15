96d74d52128e43e0ff92b6f2f8154469cbab5997
daiki8 <daiki1945815@gmail.com>
2018-08-15 01:59:51 +0000

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  attr_accesser :remember_token
+  attr_accessor :remember_token
   
   before_save { email.downcase! }
   validates :name, presence: true, length: { maximum: 50 }
