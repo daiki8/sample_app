8b658605024d34d4e6a040c083d71afdf1c613d3
daiki8 <daiki1945815@gmail.com>
2018-08-15 06:52:15 +0000

    Add test for authenticated? in user_test/rb

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -42,7 +42,9 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", user_path(@user)
     delete logout_path
     assert_not is_logged_in?
-    assert_redirected_to root_path
+    assert_redirected_to root_url
+    # 2番目のウィンドウでログアウトをクリックするユーザーをシミュレートする
+    delete logout_path
     follow_redirect!
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path, count: 0
