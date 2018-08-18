3c82ff3f768f7b6ab3b340ed56715c6e66ea61b0
daiki8 <daiki1945815@gmail.com>
2018-08-18 05:13:59 +0000

    Add Index Action

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @user = users(:michael)
+  end
+  
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -9,5 +14,24 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
     assert_select "a[href=?]", signup_path
+  end
+
+  test "layout links without logged in" do
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", users_path, count:0
+    assert_select "a[href=?]", edit_user_path(@user), count:0
+    assert_select "a[href=?]", logout_path, count:0
+    assert_select "a[href=?]", login_path
+  end
+
+  test "layout links with logged in" do
+    log_in_as(@user)
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", edit_user_path(@user)
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", login_path, count:0
   end
 end
