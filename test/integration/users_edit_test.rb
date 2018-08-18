1ea3c76c2624d675498eda38de884772220bbd29
daiki8 <daiki1945815@gmail.com>
2018-08-18 04:23:22 +0000

    Add friend-forwarding

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -34,4 +34,22 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_equal name, @user.name
     assert_equal email, @user.email
   end
+  
+  test "successfull edit with friendly forwarding" do
+    get edit_user_path(@user)
+    log_in_as(@user)
+    assert_redirected_to edit_user_url(@user)
+    name = "Foo Bar"
+    email = "foo@bar.com"
+    patch user_path(@user), params: { user: { name: name,
+                                              email: email,
+                                              password: "",
+                                              password_confirmation: "" } }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal name, @user.name
+    assert_equal email, @user.email
+    assert session[:forwarding_url].nil?
+  end
 end
