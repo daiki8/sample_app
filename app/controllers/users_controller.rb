0d094608055e4beb5c568cc2a483a03bce3eb885
daiki8 <daiki1945815@gmail.com>
2018-08-28 04:27:46 +0000

    Create micropost controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -54,14 +54,7 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password,
                                   :password_confirmation)
     end
-    
-    def logged_in_user
-      unless logged_in?
-      store_location
-      flash[:danger] = "Please log in."
-      redirect_to login_url
-      end
-    end
+
     
     def correct_user
       @user = User.find(params[:id])
